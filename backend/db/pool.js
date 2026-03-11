6fd485d2208164c06704a4b17ba66ebf12791148
Bagus Site <bagussitee@gmail.com>
2026-03-11 13:56:15 +0700

    fix: add express-rate-limit

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -2,11 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "db_toko_buku",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
